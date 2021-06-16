bd306bf769beb05571f20c706d4aeebe062175e3
gitlabsync <gitlab@fors.ru>
2021-06-16 17:05:53 +0300

    Расширение добавления моделей и роутов

--- pack_core/RUN/__tools/fast_api_init.py
+++ pack_core/RUN/__tools/fast_api_init.py
@@ -14,21 +14,42 @@ class FastAPIRouteImportError(Exception):
 
 def register_all_routes_in_app(app):
     """
-    Регистрация всех роутов приложения
+    Регистрация роутов + дополнительные пути через конфиг
+    """
+    register_routes_in_app_from_path(app)
+
+    if getattr(GeneralConfig, 'FAST_API_EXT_MODELS', None):
+        for model in GeneralConfig.FAST_API_EXT_MODELS:
+            register_routes_in_app_from_path(
+                app,
+                path_to_routes=model['path'],
+                path_to_pack=model['pack'],
+                exclude_pack=model.get('excl_routes', None)
+            )
+
+
+def register_routes_in_app_from_path(
+        app,
+        path_to_routes=GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH,
+        path_to_pack=GeneralConfig.DEFAULT_AERICH_MODEL_PACK_PATH,
+        exclude_pack=None
+):
+    """
+    Регистрация всех роутов по заданному пути
     :return:
     """
 
-    _, dirs, _ = next(walk(GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH))
+    _, dirs, _ = next(walk(path_to_routes))
 
     path_folder = []
     for name_dir in dirs:
         if name_dir.startswith('__') or name_dir.startswith('_'):
             continue
 
-        path_folder.append(GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH / name_dir)
+        path_folder.append(path_to_routes / name_dir)
 
     if len(path_folder) == 0:
-        raise FastAPIRouteImportError(f'{GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH} is empty!')
+        raise FastAPIRouteImportError(f'{path_to_routes} is empty!')
 
     result_model_path = []
 
@@ -42,7 +63,12 @@ def register_all_routes_in_app(app):
         file_path = pack.module_finder.path / pack.name / f'{DEFAULT_ROUTER_FILE_NAME}.py'
         if file_path.is_file():
 
-            path_pack = generate_pack_name(pack.module_finder.path.name, pack.name)
+            path_pack = generate_pack_name(
+                par=pack.module_finder.path.name,
+                ch=pack.name,
+                path_to_pack=path_to_pack,
+                path_to_routes=path_to_routes
+            )
             mod = import_module(
                 f'.{DEFAULT_ROUTER_FILE_NAME}',
                 path_pack
@@ -60,6 +86,6 @@ def register_all_routes_in_app(app):
     return result_model_path
 
 
-def generate_pack_name(par, ch):
+def generate_pack_name(par, ch, path_to_pack, path_to_routes):
     return \
-        f'{GeneralConfig.DEFAULT_AERICH_MODEL_PACK_PATH}.{GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH.name}.{par}.{ch}'
+        f'{path_to_pack}.{path_to_routes.name}.{par}.{ch}'
--- pack_core/RUN/__tools/tortoise.py
+++ pack_core/RUN/__tools/tortoise.py
@@ -6,6 +6,7 @@ import pkgutil
 from os import walk
 from GENERAL_CONFIG import GeneralConfig
 from pathlib import Path
+
 DEFAULT_MODEL_FILE_NAME = 'models'
 DEFAULT_PYDANTIC_FILE_NAME = 'pydmodel'
 
@@ -16,22 +17,41 @@ class TortoiseCFGModelError(Exception):
 
 def models_inspector():
     """
+    Добавление моделей
+    """
+    models_inspector_one_path()
+
+    if getattr(GeneralConfig, 'FAST_API_EXT_MODELS', None):
+        for model in GeneralConfig.FAST_API_EXT_MODELS:
+            models_inspector_one_path(
+                path_to_routes=model['path'],
+                path_to_pack=model['pack'],
+                exclude_pack=model.get('excl_models', None)
+            )
+
+
+def models_inspector_one_path(
+        path_to_routes=GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH,
+        path_to_pack=GeneralConfig.DEFAULT_AERICH_MODEL_PACK_PATH,
+        exclude_pack=None
+):
+    """
     Основная функция формирования подстановки моделей.
     Папки с префиксами __ или _ игнорируются
     :return:
     """
 
-    _, dirs, _ = next(walk(GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH))
+    _, dirs, _ = next(walk(path_to_routes))
 
     path_folder = []
     for name_dir in dirs:
         if name_dir.startswith('__') or name_dir.startswith('_'):
             continue
 
-        path_folder.append(GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH / name_dir)
+        path_folder.append(path_to_routes / name_dir)
 
     if len(path_folder) == 0:
-        raise TortoiseCFGModelError(f'{GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH} is empty!')
+        raise TortoiseCFGModelError(f'{path_to_routes} is empty!')
 
     result_model_path = []
 
@@ -42,28 +62,34 @@ def models_inspector():
         if pack.name.startswith('__') or pack.name.startswith('_'):
             continue
 
+        if exclude_pack:
+            if pack.name in exclude_pack:
+                continue
+
         file_path = pack.module_finder.path / pack.name / f'{DEFAULT_MODEL_FILE_NAME}.py'
         if file_path.is_file():
-            model_path = create_models_path(pack.module_finder.path.name, pack.name)
+            model_path = create_models_path(
+                parent=pack.module_finder.path.name,
+                module=pack.name,
+                path_to_routes=path_to_routes,
+                path_to_pack=path_to_pack
+            )
             result_model_path.append(model_path)
 
+    # Системные модели
     from ...system_models import system_models
     obj_path = [el for el in Path(system_models.__file__).parts if el not in GeneralConfig.PROJECT_GENERAL_FOLDER.parts]
-    path_to_system_model = '.'.join(obj_path)[:-3] # убираем .py
+    path_to_system_model = '.'.join(obj_path)[:-3]  # убираем .py
     result_model_path.append(path_to_system_model)
 
     return result_model_path
 
 
-def create_models_path(parent, module):
+def create_models_path(parent, module, path_to_routes, path_to_pack):
     """
     Путь к модели по регламенту тортоиса
-
-    :param parent:
-    :param module:
-    :return:
     """
-    return f'{GeneralConfig.DEFAULT_AERICH_MODEL_PACK_PATH}.\
-{GeneralConfig.DEFAULT_AERICH_MODEL_APP_PATH.name}.\
+    return f'{path_to_pack}.\
+{path_to_routes.name}.\
 {parent}.{module}.\
 {DEFAULT_MODEL_FILE_NAME}'
